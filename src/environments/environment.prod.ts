26c0f82366e9d7df1c59d5271c8ecaaa70378b6b
Lucio Mederos <luciodavid93@gmail.com>
2020-09-16 17:53:00 -0400

    Firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA_oRWZkL8xhbLhU7AgSWwoJbRVMKF96dY',
+    authDomain: 'vidlib-back.firebaseapp.com',
+    databaseURL: 'https://vidlib-back.firebaseio.com',
+    projectId: 'vidlib-back',
+    storageBucket: 'vidlib-back.appspot.com',
+    messagingSenderId: '128764598637',
+    appId: '1:128764598637:web:d58f330bb4f71523496264'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA_oRWZkL8xhbLhU7AgSWwoJbRVMKF96dY',
+    authDomain: 'vidlib-back.firebaseapp.com',
+    databaseURL: 'https://vidlib-back.firebaseio.com',
+    projectId: 'vidlib-back',
+    storageBucket: 'vidlib-back.appspot.com',
+    messagingSenderId: '128764598637',
+    appId: '1:128764598637:web:d58f330bb4f71523496264'
+  }
 };
 
 /*
