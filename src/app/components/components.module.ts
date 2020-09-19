a4a119b3a8fc93f4cf283959c286bec0a2052707
LucioD93 <luciodavid93@gmail.com>
2020-09-19 12:49:45 -0400

    Angular Material installed and added starter card (#2)

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoListItemComponent } from './video-list-item/video-list-item.component';
 
@@ -12,7 +15,9 @@ import { VideoListItemComponent } from './video-list-item/video-list-item.compon
   ],
   imports: [
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [
     VideoListComponent
